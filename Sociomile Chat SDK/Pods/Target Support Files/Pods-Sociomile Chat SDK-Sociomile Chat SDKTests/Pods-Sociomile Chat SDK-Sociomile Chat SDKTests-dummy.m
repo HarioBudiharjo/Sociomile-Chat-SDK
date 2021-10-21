70df08468f4bc86cb35079be65b7ee002ac8b573
Hario Budiharjo <hariobudiharjo@gmail.com>
2021-10-21 22:10:44 +0700

    Component

---
+++ Sociomile Chat SDK/Pods/Target Support Files/Pods-Sociomile Chat SDK-Sociomile Chat SDKTests/Pods-Sociomile Chat SDK-Sociomile Chat SDKTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Sociomile_Chat_SDK_Sociomile_Chat_SDKTests : NSObject
+@end
+@implementation PodsDummy_Pods_Sociomile_Chat_SDK_Sociomile_Chat_SDKTests
+@end
---
+++ Sociomile Chat SDK/Pods/Target Support Files/Pods-Sociomile Chat SDK/Pods-Sociomile Chat SDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Sociomile_Chat_SDK : NSObject
+@end
+@implementation PodsDummy_Pods_Sociomile_Chat_SDK
+@end
